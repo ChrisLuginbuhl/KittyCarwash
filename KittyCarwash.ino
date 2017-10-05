d382be9ff3720d4e0b2f640e01940aff8ed0781c
ChrisLuginbuhl <chris.luginbuhl@Gmail.com>
2017-10-05 19:19:13 -0400

    Changed pin #s to reflect new wiring

--- KittyCarwash.ino
+++ KittyCarwash.ino
@@ -4,47 +4,48 @@
 */
 #include <Servo.h>
 
-const int buttonPin = 6;        //In a previous version, this was Pin 9, but the docs say: "This analog input is connected to a voltage divider for 
-                                // the lipoly battery so be aware that this pin naturally 'sits' at around 2VDC due to the resistor divider". Switched to pin 6. 
-const int servoPin = 10;
+const int buttonPin = 13;          //In a previous version, this was Pin 9, but the docs say: "This analog input is connected to a voltage divider for
+// the lipoly battery so be aware that this pin naturally 'sits' at around 2VDC due to the resistor divider". Switched to pin 6.
+const int servoPin = 6;
 
-int buttonState = LOW;          //Initialize a variable to track whether the button is pressed
-int pos = 1;                    //Servo position in degrees
+int buttonState = LOW;            //Initialize a variable to track whether the button is pressed
+int pos = 1;                      //Servo position in degrees
 
-Servo servoOne;                   //Create a servo object 
+Servo servoOne;                   //Create a servo object
 
 void setup()
 {
-  
-  servoOne.attach(10);             //Servos are connected to the same pin for their control signal
-  pinMode(buttonPin, INPUT_PULLUP); //When the switch is open, the buttonPin voltage floats. Adding PULLUP ensures it goes to HIGH
+
+  servoOne.attach(servoPin);                //Servos are connected to the same pin for their control signal
+  pinMode(buttonPin, INPUT);                //When the switch is open, the buttonPin voltage floats. Adding PULLUP ensures it goes to HIGH
 }
 
 void loop()
 {
- // Serial.println(buttonState);            //This is for test & debug purposes. We can use Serial Monitor to see what state the Arduino sees the switch in.
-    buttonState = digitalRead(buttonPin);
-    
-    while(buttonState == HIGH) { 
-      /*Check the buttonState once for each loop. If it's HIGH, take the servomotor from 1 to 60 degrees and back to 1,
-       * and then recheck the buttonState and repeat. 
-       * 
-       * Returning to 1 degree ensures the servo is always "parked" in the same position, and avoids a sudden full-power lurch when the 
-       * arduino is connected to power.
-       * 
-       * We used 1 degree rather than 0 because one of our test servos was straining against
-       * the hard stop when set to zero.
-       */
-    
-      for (pos = 1; pos < 60; pos++) {
-        servoOne.write(pos);
-        delay(2);  //delay 2ms to ensure servo has time to complete the move
-      }
-      
-      for (pos = 60; pos > 2; pos--) {
-        servoOne.write(pos);
-        delay(2);
-      }
-    buttonState = digitalRead(buttonPin);
+  // Serial.println(buttonState);            //This is for test & debug purposes. We can use Serial Monitor to see what state the Arduino sees the switch in.
+  buttonState = digitalRead(buttonPin);
+
+  while (buttonState == HIGH) {
+    /*Check the buttonState once for each loop. If it's HIGH, take the servomotor from 1 to 60 degrees and back to 1,
+       and then recheck the buttonState and repeat.
+
+       Returning to 1 degree ensures the servo is always "parked" in the same position, and avoids a sudden full-power lurch when the
+       arduino is connected to power.
+
+       We used 1 degree rather than 0 because one of our test servos was straining against
+       the hard stop when set to zero.
+    */
+
+    for (pos = 1; pos < 60; pos++) {
+      servoOne.write(pos);
+      delay(2);                         //delay 2ms to ensure servo has time to complete the move
     }
+
+    for (pos = 60; pos > 2; pos--) {
+      servoOne.write(pos);
+      delay(2);
+    }
+    buttonState = digitalRead(buttonPin);
+    Serial.println(buttonState);
+  }
 }
